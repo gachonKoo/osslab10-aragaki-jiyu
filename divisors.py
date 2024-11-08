3e3439e2c499ee2e65437ea29a49d2139376dcf0
aragaki-jiyu <raphaelkang@kakao.com>
2024-11-08 23:03:25 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,8 +1,7 @@
 import sys
-x = sys.stdin.readline()
-x = int(x)
+x = int(sys.argv[1])
 y = list()
-for i in range(1,t):
-  if (t % i == 0):
+for i in range(1,x):
+  if (x % i == 0):
     y.append(i)
 print(*y)  
